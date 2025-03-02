095e87ee50f9d2fa3aa8bfe76455948bd21d8845
Pritam Mandal <pritammandal1717@gmail.com>
2025-03-02 19:51:31 +0530

    Adding prisma to express app

--- Docker/DockerIntro/src/index.ts
+++ Docker/DockerIntro/src/index.ts
@@ -1,9 +1,26 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express'
 
 const app = express();
 
+app.use(express.json())
+
+const prisma = new PrismaClient();
+
 app.get("/", (req, res) => {
     res.json({message : "huii"});
+})
+
+app.post("/sign",async  (req, res) => {
+    const body = req.body;
+    console.log(body)
+    const user = await prisma.user.create({
+        data : {
+            name : body.name,
+            email : body.email
+        }
+    })
+    res.json({user})
 })
 
 app.listen(3000, () => {
